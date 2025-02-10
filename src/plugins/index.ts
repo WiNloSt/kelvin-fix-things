a1cd60a48360279d55590334d601c7abb9afa084
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-02-11 00:26:30 +0700

    Remove unnesecessary env

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -102,7 +102,7 @@ export const plugins: Plugin[] = [
         accessKeyId: process.env.R2_ACCESS_KEY_ID || '',
         secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || '',
       },
-      region: process.env.R2_REGION,
+      region: 'auto',
       endpoint: process.env.R2_ENDPOINT,
     },
   }),
