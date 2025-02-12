26b69d9ffa7074fb3876f17b420aba04043d3c08
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-02-12 19:54:38 +0700

    Remove console.log

--- src/blocks/Column/Component.tsx
+++ src/blocks/Column/Component.tsx
@@ -6,7 +6,6 @@ import RichText from '@/components/RichText'
 type Props = {} & ColumnBlockProps
 
 export const ColumnBlock: React.FC<Props> = ({ leftColumn, rightColumn }) => {
-  console.log({ leftColumn, rightColumn })
   return (
     <div className="grid auto-cols-fr md:grid-flow-col md:gap-8 -my-5">
       <RichText data={leftColumn} enableGutter={false} enableProse={false} />
