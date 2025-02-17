0ca88d0de489b550815949144e62cba1070f0124
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2025-02-17 09:28:15 +0700

    Fix pages, posts autosave too fast causing duplicate records

--- src/collections/Pages/index.ts
+++ src/collections/Pages/index.ts
@@ -135,7 +135,7 @@ export const Pages: CollectionConfig<'pages'> = {
   versions: {
     drafts: {
       autosave: {
-        interval: 100, // We set this interval for optimal live preview
+        interval: 800, // default value
       },
       schedulePublish: true,
     },
